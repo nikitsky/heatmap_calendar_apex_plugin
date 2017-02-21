4d0daec2149a612bbcd0336839d7b9ddcbf7a9dc
Valentine Nikitsky <nikitsky@gmail.com>
2017-02-21 14:51:29 +0100

    heatmap_calendar is a javascript function now

--- src/heatmap_calendar.js
+++ src/heatmap_calendar.js
@@ -1,87 +1,129 @@
-var
-    // cellSize = 17,
-    week_days = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'],
-    month = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+function heatmap_calendar( pRegionId, pOptions ) {
+    var gOptions = jQuery.extend(
+        {
+            cellSize: 16,
+            years: [2016, 2017, 2018],
+            day_caption: ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'],
+		    month_caption: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+		    DateFormatMask: "%d.%m.%Y",
+		    calendarClass: "RdYlGn"
+        },
+        pOptions
+    );
 
+    var gRegion$ = jQuery( "#" + apex.util.escapeCSS( pRegionId ) + '_hc', apex.gPageContext$);
 
-
-var day = function(d) {
-		return ( d.getDay() == 0 ) ? 6: d.getDay()-1;
-	}
+	var day = function(d) {
+			return ( d.getDay() == 0 ) ? 6: d.getDay()-1;
+		}
     week = d3.timeFormat("%W"),
+    format = d3.timeFormat(gOptions.DateFormatMask);
+    width = gOptions.cellSize * 53 + 51;
+    height = gOptions.cellSize * 7 + 31;
 
-    parseDate = d3.timeParse("%d.%m.%Y"),
-    format = d3.timeFormat("%Y-%m-%d");
-    // format = d3.timeFormat("%d.%m.%Y");
 
-    width = cellSize * 53 + 51;
-    height = cellSize * 7 + 31;
+    _draw ([]);
 
-var svg = d3.select("#" + regionID + '_hc').selectAll("svg")
-    .data(d3.range(2016, 2019))
-  .enter().append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("class", "RdYlGn")
-  .append("g") //allign calendar middle-bottom
-    // .attr("transform", "translate(" + ((width - cellSize * 53) / 2) + "," + (height - cellSize * 7 - 1) + ")");
-    .attr("transform", "translate(50, 30)");
 
-	//caption: year
-	svg.append("text")
-    // .attr("transform", "translate(-36," + cellSize * 3.5 + ")rotate(-90)")
-	    .attr("dy", "-.25em")
-	    .attr("class", "year_caption")
-	    .text(function(d) { return d; });
+	function _draw( pData ) {
+		var svg = d3.select( "#" + apex.util.escapeCSS( pRegionId ) + '_hc' ).selectAll("svg")
+		    .data(gOptions.years)
+		  	.enter().append("svg")
+				    .attr("width", width)
+				    .attr("height", height)
+				    .attr("class", gOptions.calendarClass)
+				.append("g")
+				    .attr("transform", "translate(50, 30)");
 
-	//caption: weekdays
-	var weekdays = svg.selectAll(".days_caption")
-		.data (d3.range(0,7))
-		.data ( week_days )
-		.enter().append("text")
-			.attr("class", "days_caption")
-			.attr("x", "-5")
-		    .attr("y", function(d, i) { return cellSize*(i+1); })
-		    // .attr("transform", function(d, i) { return "translate(-5," + cellSize*(i+1) + ")"; })
+		//caption: year
+		svg.append("text")
+	    // .attr("transform", "translate(-36," + cellSize * 3.5 + ")rotate(-90)")
 		    .attr("dy", "-.25em")
-		    .text(function(d) { return d ; });
+		    .attr("class", "year_caption")
+		    .text(function(d) { return d; });
 
-	//capton: month
-	var legend = svg.selectAll(".months_caption")
-		.data(month)
-		.enter().append("text")
-			.attr("class", "months_caption")
-			.attr("x", function(d, i) { return ((i+1) * (cellSize * 53 / 12)-cellSize*1.5) ; })
-			.attr("dy", "-.5em")
-			.text(function(d,i){ return d });
+		//caption: weekdays
+		var weekdays = svg.selectAll(".day_caption")
+			.data (d3.range(0,7))
+			.data ( gOptions.day_caption )
+			.enter().append("text")
+				.attr("class", "day_caption")
+				.attr("x", "-5")
+			    .attr("y", function(d, i) { return gOptions.cellSize*(i+1); })
+			    .attr("dy", "-.25em")
+			    .text(function(d) { return d ; });
 
-	// days
-	var rect = svg.selectAll(".day")
-	    .data(function(d) { return d3.timeDay.range(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
-	  .enter().append("rect")
-	    .attr("class", "day")
-	    .attr("width", cellSize)
-	    .attr("height", cellSize)
-	    .attr("x", function(d) { return week(d) * cellSize; })
-	    .attr("y", function(d) { return day(d) * cellSize; })
-	    .datum(format);
+		//capton: month
+		var month_captions = svg.selectAll(".month_caption")
+			.data(gOptions.month_caption)
+			.enter().append("text")
+				.attr("class", "month_caption")
+				.attr("x", function(d, i) { return ((i+1) * (gOptions.cellSize * 53 / 12)-gOptions.cellSize*1.5) ; })
+				.attr("dy", "-.5em")
+				.text(function(d,i){ return d });
 
-	rect.append("title")
-	    .text(function(d) { return d; });
+		// days
+		var rect = svg.selectAll(".day")
+			.data(function(d) { return d3.timeDay.range(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+		  	.enter().append("rect")
+			    .attr("class", "day")
+			    .attr("width", gOptions.cellSize)
+			    .attr("height", gOptions.cellSize)
+			    .attr("x", function(d) { return week(d) * gOptions.cellSize; })
+			    .attr("y", function(d) { return day(d) * gOptions.cellSize; })
+			    .datum(format);
 
-	svg.selectAll(".month")
-    	.data(function(d) { return d3.timeMonths(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
-  		.enter().append("path")
-    		.attr("class", "month")
-    		.attr("d", monthPath);
+		// put date to the cell
+		rect.append("title")
+		    .text(function(d) { return d; });
 
-	function monthPath(t0) {
-	  var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
-	      d0 = +day(t0), w0 = +week(t0),
-	      d1 = +day(t1), w1 = +week(t1);
-	  return "M" + (w0 + 1) * cellSize + "," + d0 * cellSize
-	      + "H" + w0 * cellSize + "V" + 7 * cellSize
-	      + "H" + w1 * cellSize + "V" + (d1 + 1) * cellSize
-	      + "H" + (w1 + 1) * cellSize + "V" + 0
-	      + "H" + (w0 + 1) * cellSize + "Z";
+		// draw months borders
+		svg.selectAll(".month")
+	    	.data(function(d) { return d3.timeMonths(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+	  		.enter().append("path")
+	    		.attr("class", "month")
+	    		.attr("d", monthPath);
+
+		function monthPath(t0) {
+		  var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
+		      d0 = +day(t0), w0 = +week(t0),
+		      d1 = +day(t1), w1 = +week(t1);
+		  return "M" + (w0 + 1) * gOptions.cellSize + "," + d0 * gOptions.cellSize
+		      + "H" + w0 * gOptions.cellSize + "V" + 7 * gOptions.cellSize
+		      + "H" + w1 * gOptions.cellSize + "V" + (d1 + 1) * gOptions.cellSize
+		      + "H" + (w1 + 1) * gOptions.cellSize + "V" + 0
+		      + "H" + (w0 + 1) * gOptions.cellSize + "Z";
+		}
 	}
+
+
+    function _refresh() {
+        apex.server.plugin(
+            gOptions.ajaxIdentifier,
+            {
+                pageItems: gOptions.pageItems
+            },
+            {
+                dataType: "json",
+                accept: "application/json",
+                refreshObject: gRegion$,
+                success: _draw,
+                error:  _debug
+            }
+        );
+    }
+
+    function _debug( i ) {
+        apex.debug.log( i );
+    }
+
+
+    // gRegion$
+    //     .on( "apexrefresh", _refresh )
+    //     .trigger( "apexrefresh" );
+}
+
+
+
+
+
--- src/render_region_source.sql
+++ src/render_region_source.sql
@@ -15,19 +15,33 @@ begin
                        p_directory      => p_plugin.file_prefix,
                        p_skip_extension => false);
 
-    -- Output the container for the calendar which is used by the Javascript code
-    sys.htp.p('<div id='||apex_javascript.add_value(sys.htf.escape_sc(p_region.static_id||'_hc'), false)||'></div>');
-
-    -- define variables for calendar
-    sys.htp.p('<script type="text/javascript">');
-    sys.htp.p('var cellSize = '||apex_javascript.add_value(sys.htf.escape_sc(l_SellSize), true));
-    sys.htp.p('regionID = '||apex_javascript.add_value(sys.htf.escape_sc(p_region.static_id), false)||';');
-    sys.htp.p('</script>');
-
     apex_javascript.add_library( p_name                  => 'heatmap_calendar',
                                  p_directory             => p_plugin.file_prefix,
                                  p_check_to_add_minified => false );
 
+    -- Output the container for the calendar which is used by the Javascript code
+    sys.htp.p('<div id='||apex_javascript.add_value(sys.htf.escape_sc(p_region.static_id||'_hc'), false)||'></div>');
+
+    -- define variables for calendar
+    apex_javascript.add_onload_code (
+        p_code => 'heatmap_calendar('||
+            apex_javascript.add_value(p_region.static_id)||
+            '{'||
+                apex_javascript.add_attribute(
+                    p_name      => 'ajaxIdentifier',
+                    p_value     => apex_plugin.get_ajax_identifier,
+                    p_omit_null => false,
+                    p_add_comma => true
+                )||
+                apex_javascript.add_attribute(
+                    p_name      => 'cellSize',
+                    p_value     => sys.htf.escape_sc(l_SellSize),
+                    p_omit_null => true,
+                    p_add_comma => false
+                )||
+            '}'||
+        ');'
+    );
 
     return null;
 
